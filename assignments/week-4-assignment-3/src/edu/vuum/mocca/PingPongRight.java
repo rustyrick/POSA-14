02bec2997be1206f92c51325f67971a78abe0e3a
Francesco Novellis <rustyrick@gmail.com>
2014-06-16 01:49:49 +0200

    week 4 assignment 3

--- assignments/week-4-assignment-3/src/edu/vuum/mocca/PingPongRight.java
+++ assignments/week-4-assignment-3/src/edu/vuum/mocca/PingPongRight.java
@@ -49,13 +49,15 @@ public class PingPongRight {
          * iteration.
          */
         // TODO - You fill in here.
-
+        private String mStringToPrint;
         /**
          * Two SimpleSemaphores use to alternate pings and pongs.  You
          * can use an array of SimpleSemaphores or just define them as
          * two data members.
          */
         // TODO - You fill in here.
+        private SimpleSemaphore mSem1;
+        private SimpleSemaphore mSem2;
 
         /**
          * Constructor initializes the data member(s).
@@ -65,6 +67,11 @@ public class PingPongRight {
                                   SimpleSemaphore semaphoreTwo,
                                   int maxIterations) {
             // TODO - You fill in here.
+        	mStringToPrint = stringToPrint;
+        	mSem1 = semaphoreOne;
+        	mSem2 = semaphoreTwo;
+        	mMaxLoopIterations = maxIterations;
+        	mLatch = new CountDownLatch(2);
         }
 
         /**
@@ -79,6 +86,13 @@ public class PingPongRight {
              */
 
             // TODO - You fill in here.
+        	for(int loopsDone = 1; loopsDone <= mMaxLoopIterations; ++loopsDone)
+        	{
+        		acquire();
+        		System.out.println(mStringToPrint+"("+loopsDone+")");
+        		release();
+        	}
+        	mLatch.countDown();
         }
 
         /**
@@ -86,6 +100,7 @@ public class PingPongRight {
          */
         private void acquire() {
             // TODO fill in here
+        	mSem1.acquireUninterruptibly();
         }
 
         /**
@@ -93,6 +108,7 @@ public class PingPongRight {
          */
         private void release() {
             // TODO fill in here
+        	mSem2.release();
         }
     }
 
@@ -107,37 +123,39 @@ public class PingPongRight {
 
         // TODO initialize this by replacing null with the appropriate
         // constructor call.
-        mLatch = null;
+        mLatch = new CountDownLatch(maxIterations);
 
         // Create the ping and pong SimpleSemaphores that control
         // alternation between threads.
 
         // TODO - You fill in here, make pingSema start out unlocked.
-        SimpleSemaphore pingSema = null;
+        SimpleSemaphore pingSema = new SimpleSemaphore(1, true);
         // TODO - You fill in here, make pongSema start out locked.
-        SimpleSemaphore pongSema = null;
+        SimpleSemaphore pongSema = new SimpleSemaphore(0, true);;
 
         System.out.println(startString);
 
         // Create the ping and pong threads, passing in the string to
         // print and the appropriate SimpleSemaphores.
-        PlayPingPongThread ping = new PlayPingPongThread(/*
+        PlayPingPongThread ping = new PlayPingPongThread(pingString, pingSema, pongSema, maxIterations/*
                                                           * TODO - You fill in
                                                           * here
                                                           */);
-        PlayPingPongThread pong = new PlayPingPongThread(/*
+        PlayPingPongThread pong = new PlayPingPongThread(pongString, pongSema, pingSema, maxIterations/*
                                                           * TODO - You fill in
                                                           * here
                                                           */);
 
         // TODO - Initiate the ping and pong threads, which will call
         // the run() hook method.
+        ping.start();
+        pong.start();
 
         // TODO - replace the following line with a barrier
         // synchronizer call to mLatch that waits for both threads to
         // finish.
-        throw new java.lang.InterruptedException();
-
+        //throw new java.lang.InterruptedException();
+        mLatch.await();
         System.out.println(finishString);
     }
 
